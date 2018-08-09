e5e9ea0ac2348db47750d5adbe8a2d6974630784
AIR <air@AIRdeMacBook-Air.local>
2018-08-09 22:37:32 +0800

    plan

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -25,6 +25,77 @@ class TestController extends Yaf_Controller_Abstract
 
     }
 
+    public function planAction(){
+        $subjectId = 1;
+        $courseName = '3';
+        $courseVersion = '1';
+        $level = '2';// 1-周 2-2周 3-3周  5-月 6-日
+        $times = '2';//count($hope) 个数比对即可
+        $once = '45';//0.5 0.75 1 1.5 2 ....
+        $plan_start = '2018-08-08';
+        $plan_end = '2018-10-08';
+
+        $hope['1'] = [//第一周的周一
+            'start'=>'18:00:00',
+            'end'=>'20:00:00'
+        ];
+
+        $hope['11'] = [//第二周的周四
+            'start'=>'19:00:00',
+            'end'=>'21:00:00'
+        ];
+
+        $first_xq = date("w",strtotime($plan_start));//排课开始第一天是周几
+        $first_week = 1; //以周几为周的第一天 1-周一 0-周日
+
+        $flag = 1;//第几周
+        if($first_xq==$first_week){
+            $flag = 0;//第几周
+        }
+        $course_times = 0;//总排课几次
+        for($i=strtotime($plan_start);$i<=strtotime($plan_end);$i+=86400){
+            echo date("Ymd", $i) . '------' . $flag . '======';
+
+            if($level==5){ //指定具体几号
+                $day = intval(date("d", $i));
+                if($hope[$day]){
+                    echo date("Ymd", $i) . ' ' . $hope[$day]['start'] . '-' . date("Ymd", $i) . ' ' . $hope[$day]['end'];
+                    $course_times++;
+                }
+            } elseif($level==6){ //指定具体日期
+                foreach($hope as $key => $val){
+                    if(date("Ymd", $i) == date("Ymd", strtotime($key))){
+                        echo date("Ymd", $i) . ' ' .$val['start'] . '-' . date("Ymd", $i) . ' ' . $val['end'];
+                        $course_times++;
+                    }
+                }
+
+            } else {
+
+                $xq = date("w", $i);
+
+                if ($xq == $first_week) {
+                    $flag++;
+                }
+
+                foreach ($hope as $key => $val) {
+                    $this_xq = $key%7;//周几
+                    $week = ceil($key/7);//第几周
+                    if ($week == $level) {
+                        $week = 0;
+                    }
+                    if (($flag % $level == $week) && $this_xq == $xq) {
+                        echo date("Ymd", $i) . ' ' . $val['start'] . '-' . date("Ymd", $i) . ' ' . $val['end'];
+                        $course_times++;
+                    }
+                }
+            }
+            echo "<hr>";
+        }
+        echo $course_times;
+        die;
+    }
+
 
     private function log($content='') {
         $fp = fopen('/tmp/test_book.log','a');
